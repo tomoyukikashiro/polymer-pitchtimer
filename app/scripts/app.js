d12695b418beaed4139ecdbde1a9c30cee1de143
tomoyuki kashiro <ksrtmyk@gmail.com>
2014-12-24 22:36:38 +0900

    add skelton

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,10 +2,6 @@
   'use strict';
 
   document.addEventListener('polymer-ready', function() {
-    // Perform some behaviour
-    console.log('Polymer is ready to rock!');
   });
 
-// wrap document so it plays nice with other libraries
-// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
